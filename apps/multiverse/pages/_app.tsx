ffc29f04b0f93e7d6efd60605c142ba31c6fb33c
Amir.H Ebrahimi <ah.ebrahimi@phinix.com>
2022-07-28 16:58:45 +0430

    chore: Prevent enforce-module-boundaries

--- apps/multiverse/pages/_app.tsx
+++ apps/multiverse/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import '@multiverse/src/styles/tailwind.css';
+import '~styles/tailwind.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
